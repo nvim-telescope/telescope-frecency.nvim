3c45690c1e322dbaa9d85df739ff6d2d87ce2654
Senghan Bright <senghan.bright@deltaprojects.com>
2021-01-26 18:11:25 +0100

    wip LSP workspace filter

--- lua/telescope/_extensions/frecency.lua
+++ lua/telescope/_extensions/frecency.lua
@@ -1,11 +1,9 @@
 local has_telescope, telescope = pcall(require, "telescope")
 
--- TODO: make dependency errors occur in a better way
 if not has_telescope then
   error("This plugin requires telescope.nvim (https://github.com/nvim-telescope/telescope.nvim)")
 end
 
--- finder code
 local actions       = require('telescope.actions')
 local conf          = require('telescope.config').values
 local entry_display = require "telescope.pickers.entry_display"
@@ -14,10 +12,10 @@ local path          = require('telescope.path')
 local pickers       = require "telescope.pickers"
 local sorters       = require "telescope.sorters"
 local utils         = require('telescope.utils')
+local db_client     = require("telescope._extensions.frecency.db_client")
 
 local os_home       = vim.loop.os_homedir()
 local os_path_sep   = utils.get_separator()
-local db_client
 
 local state = {
   results         = {},
@@ -26,18 +24,43 @@ local state = {
   cwd             = nil,
   show_scores     = false,
   user_workspaces = {},
+  lsp_workspaces  = {}
 }
 
-local get_workspace_tags = function()
-  -- local lsp_workspace = vim.api.nvim_buf_call(state.previous_buffer, vim.lsp.buf.list_workspace_folders)
-  -- print(vim.inspect(state.workspaces))
-  -- return vim.tbl_extend(state.workspaces, {["LSP"] = lsp_workspace[1]})
+local function format_filepath(filename, opts)
+  local original_filename = filename
 
+  if opts.tail_path then
+    filename = utils.path_tail(filename)
+  elseif opts.shorten_path then
+    filename = utils.path_shorten(filename)
+  else -- check relative to home/current
+    filename = path.make_relative(filename, state.cwd)
+    if vim.startswith(filename, os_home) then
+      filename = "~/" ..  path.make_relative(filename, os_home)
+    elseif filename ~= original_filename then
+      filename = "./" .. filename
+    end
+  end
+
+  return filename
+end
+
+local function get_workspace_tags()
   -- TODO: validate that workspaces are existing directories
   local tags = {}
   for k,_ in pairs(state.user_workspaces) do
     table.insert(tags, k)
   end
+  local lsp_workspaces = vim.api.nvim_buf_call(state.previous_buffer, vim.lsp.buf.list_workspace_folders)
+
+  if not vim.tbl_isempty(lsp_workspaces) then
+    state.lsp_workspaces = lsp_workspaces
+    tags[#tags+1] = "LSP_ROOT"
+  else
+    state.lsp_workspaces = {}
+  end
+
   -- print(vim.inspect(tags))
   -- TODO: sort tags - by collective frecency?
   return tags
@@ -47,7 +70,7 @@ local frecency = function(opts)
   opts = opts or {}
 
   state.previous_buffer = vim.fn.bufnr('%')
-  state.cwd = vim.fn.expand(opts.cwd or vim.fn.getcwd())
+  -- state.cwd = vim.fn.expand(opts.cwd or vim.fn.getcwd())
 
   local display_cols = {}
   display_cols[1] = state.show_scores and {width = 8} or nil
@@ -59,31 +82,16 @@ local frecency = function(opts)
     items = display_cols
   }
 
-  local bufnr, buf_is_loaded, filename, hl_filename, display_items, original_filename
-
+  local bufnr, buf_is_loaded, filename, hl_filename, display_items
   local make_display = function(entry)
     bufnr = vim.fn.bufnr
     buf_is_loaded = vim.api.nvim_buf_is_loaded
 
-    filename = entry.name
+    filename    = entry.name
     hl_filename = buf_is_loaded(bufnr(filename)) and "TelescopeBufferLoaded" or ""
+    filename    = format_filepath(filename, opts)
 
-    original_filename = filename
-
-    if opts.tail_path then
-      filename = utils.path_tail(filename)
-    elseif opts.shorten_path then
-      filename = utils.path_shorten(filename)
-    else -- check relative to home/current
-      filename = path.make_relative(filename, state.cwd)
-      if vim.startswith(filename, os_home) then
-        filename = "~/" ..  path.make_relative(filename, os_home)
-      elseif filename ~= original_filename then
-        filename = "./" .. filename
-      end
-    end
-
-    display_items = state.show_scores and {{entry.score, "FoldColumn"}} or {}
+    display_items = state.show_scores and {{entry.score, "TelescopeFrecencyScores"}} or {}
     table.insert(display_items, {filename, hl_filename})
 
     return displayer(display_items)
@@ -94,8 +102,9 @@ local frecency = function(opts)
 
     -- validate tag
     local ws_dir = filter and state.user_workspaces[filter]
-    -- if filter == "lsp" then
-    -- end
+    if filter == "LSP_ROOT" and not vim.tbl_isempty(state.lsp_workspaces) then
+      ws_dir = state.lsp_workspaces[1]
+    end
 
     if ws_dir ~= state.active_filter then
       filter_updated = true
@@ -176,15 +185,14 @@ end
 
 return telescope.register_extension {
   setup = function(ext_config)
-    state.show_scores       = ext_config.show_scores or false
-    state.user_workspaces  = ext_config.workspaces or {}
+    state.show_scores     = ext_config.show_scores or false
+    state.user_workspaces = ext_config.workspaces or {}
 
     -- start the database client
-    db_client = require("telescope._extensions.frecency.db_client")
     db_client.init(ext_config.ignore_patterns)
   end,
   exports = {
-    frecency       = frecency,
+    frecency           = frecency,
     get_workspace_tags = get_workspace_tags,
   },
 }
