d8e2058dbef31746556c1f34262e7d4d6afa3c9a
delphinus <me@delphinus.dev>
2023-07-30 07:47:41 +0900

    test: add benchmarking tests

--- lua/frecency/tests/frecency_spec.lua
+++ lua/frecency/tests/frecency_spec.lua
@@ -1,6 +1,8 @@
 ---@diagnostic disable: invisible
 local Frecency = require "frecency.frecency"
 local util = require "frecency.tests.util"
+local Path = require "plenary.path"
+local log = require "plenary.log"
 
 local function prepare(files)
   local dir, close = util.make_tree(files)
@@ -11,6 +13,9 @@ local function filepath(dir, file)
   return dir:joinpath(file):absolute()
 end
 
+---@param frecency Frecency
+---@param dir PlenaryPath
+---@return fun(file: string, datetime: string, reset: boolean?): nil
 local function make_register(frecency, dir)
   return function(file, datetime, reset)
     local path = filepath(dir, file)
@@ -20,6 +25,30 @@ local function make_register(frecency, dir)
       frecency.buf_registered[bufnr] = nil
     end
     frecency:register(bufnr, datetime)
+  end
+end
+
+---comment
+---@param frecency Frecency
+---@param dir PlenaryPath
+---@return fun(file: string, datetime: string): nil faked_register
+---@return fun(): nil restore
+local function make_fake_register(frecency, dir)
+  local bufnr = 0
+  local buffers = {}
+  local original_nvim_buf_get_name = vim.api.nvim_buf_get_name
+  ---@diagnostic disable-next-line: redefined-local, duplicate-set-field
+  vim.api.nvim_buf_get_name = function(bufnr)
+    return buffers[bufnr]
+  end
+  return function(file, datetime)
+    local path = filepath(dir, file)
+    Path.new(path):touch()
+    bufnr = bufnr + 1
+    buffers[bufnr] = path
+    frecency:register(bufnr, datetime)
+  end, function()
+    vim.api.nvim_buf_get_name = original_nvim_buf_get_name
   end
 end
 
@@ -94,6 +123,35 @@ describe("frecency", function()
         }, results)
       end)
       close()
+    end)
+  end)
+
+  describe("benchmark", function()
+    describe("after registered over >5000 files", function()
+      local frecency, dir, close = prepare {}
+      local faked_register, restore = make_fake_register(frecency, dir)
+      local file_count = 6000
+      local expected = {}
+      for i = 1, file_count do
+        local file = ("hoge%08d.txt"):format(i)
+        table.insert(expected, { count = 1, id = i, path = filepath(dir, file), score = 10 })
+        faked_register(file, "2023-07-29T00:00:00+09:00")
+      end
+      local start = os.clock()
+      local results = frecency.picker:fetch_results(nil, "2023-07-29T00:01:00+09:00")
+      table.sort(results, function(a, b)
+        return a.path < b.path
+      end)
+      local elapsed = os.clock() - start
+      log.info(("it takes %f seconds in fetching all results"):format(elapsed))
+      it("returns appropriate latency (<1.0 second)", function()
+        assert.are.is_true(elapsed < 1.0)
+      end)
+      it("returns valid response", function()
+        assert.are.same(expected, results)
+      end)
+      close()
+      restore()
     end)
   end)
 end)
