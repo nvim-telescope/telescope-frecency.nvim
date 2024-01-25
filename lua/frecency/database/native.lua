299dd72563f694c6c2081802ce0d6ca4372d753c
delphinus <me@delphinus.dev>
2024-01-25 19:05:16 +0900

    fix: filter out paths validly in Windows

--- lua/frecency/database/native.lua
+++ lua/frecency/database/native.lua
@@ -115,7 +115,7 @@ function Native:get_entries(workspace, datetime)
   local now = self:now(datetime)
   local items = {}
   for path, record in pairs(self.table.records) do
-    if not workspace or path:find(workspace .. "/", 1, true) then
+    if not workspace or path:find(workspace .. Path.path.sep, 1, true) then
       table.insert(items, {
         path = path,
         count = record.count,
