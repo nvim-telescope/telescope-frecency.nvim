88050e7a8e6521ff25709fd9f130874fce34123c
delphinus <me@delphinus.dev>
2024-01-30 14:38:08 +0900

    test: test database as native

--- lua/frecency/tests/database_spec.lua
+++ lua/frecency/tests/database_spec.lua
@@ -1,41 +1,41 @@
 local FS = require "frecency.fs"
-local Native = require "frecency.database.native"
+local Database = require "frecency.database"
 local async = require "plenary.async" --[[@as PlenaryAsync]]
 local util = require "frecency.tests.util"
 async.tests.add_to_env()
 
-local function with_native(f)
+local function with_database(f)
   local fs = FS.new { ignore_patterns = {} }
   local dir, close = util.tmpdir()
   dir:joinpath("file_frecency.bin"):touch()
   return function()
-    local native = Native.new(fs, { root = dir.filename })
-    f(native)
+    local database = Database.new(fs, { root = dir.filename })
+    f(database)
     close()
   end
 end
 
-local function save_and_load(native, tbl, datetime)
-  native:raw_save(util.v1_table(tbl))
+local function save_and_load(database, tbl, datetime)
+  database:raw_save(util.v1_table(tbl))
   async.util.sleep(100)
-  local entries = native:get_entries(nil, datetime)
+  local entries = database:get_entries(nil, datetime)
   table.sort(entries, function(a, b)
     return a.path < b.path
   end)
   return entries
 end
 
-a.describe("frecency.database.native", function()
+a.describe("frecency.database", function()
   a.describe("updated by another process", function()
     a.it(
       "returns valid entries",
-      with_native(function(native)
+      with_database(function(database)
         assert.are.same(
           {
             { path = "hoge1.txt", count = 1, ages = { 60 } },
             { path = "hoge2.txt", count = 1, ages = { 60 } },
           },
-          save_and_load(native, {
+          save_and_load(database, {
             ["hoge1.txt"] = { count = 1, timestamps = { "2023-08-21T00:00:00+0000" } },
             ["hoge2.txt"] = { count = 1, timestamps = { "2023-08-21T00:00:00+0000" } },
           }, "2023-08-21T01:00:00+0000")
