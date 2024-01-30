a61ede8740643f09e1a7706fbb49b152e8f25d42
JINNOUCHI Yasushi <me@delphinus.dev>
2024-01-31 08:45:26 +0900

    feat: add healthcheck for rg/fdfind/fd (#173)

--- lua/telescope/_extensions/frecency.lua
+++ lua/telescope/_extensions/frecency.lua
@@ -8,6 +8,15 @@ return require("telescope").register_extension {
     else
       vim.health.info "nvim-web-devicons is not installed."
     end
+    if vim.fn.executable "rg" == 1 then
+      vim.health.ok "ripgrep installed."
+    elseif vim.fn.executable "fdfind" == 1 then
+      vim.health.ok "fdfind installed."
+    elseif vim.fn.executable "fd" == 1 then
+      vim.health.ok "fd installed."
+    else
+      vim.health.info "No suitable find executable found."
+    end
   end,
   exports = {
     frecency = frecency.start,
