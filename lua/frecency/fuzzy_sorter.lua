64c022904871143ab12c7d6ba29c89fbabdbe15e
delphinus <me@delphinus.dev>
2024-04-16 21:40:54 +0900

    feat: separate logic to match: fuzzy, fuzzy_full

--- lua/frecency/fuzzy_sorter.lua
+++ lua/frecency/fuzzy_sorter.lua
@@ -1,4 +1,6 @@
+local config = require "frecency.config"
 local sorters = require "telescope.sorters"
+local Path = require "plenary.path"
 
 ---@type table<string, string[]>
 local regexp_cache = {}
@@ -17,11 +19,25 @@ local function regexps(prompt)
   return regexp_cache[prompt]
 end
 
+---@type table<string, string>
+local target_cache = {}
+local basename_re = ("[^%s]*$"):format(Path.path.sep)
+
+---@param filename string
+---@return string
+local function target(filename)
+  if not target_cache[filename] then
+    target_cache[filename] = config.matcher == "fuzzy" and filename:match(basename_re) or filename
+  end
+  return target_cache[filename]
+end
+
 return sorters.Sorter:new {
   ---@param prompt string
   ---@param display string
   highlighter = function(_, prompt, display)
-    local converted = display:lower()
+    local converted = target(display):lower()
+    local offset = #display - #converted
     local res = regexps(prompt)
     ---@type { start: number, finish: number }[]
     local highlights = {}
@@ -30,7 +46,7 @@ return sorters.Sorter:new {
       local start, finish = converted:find(re, init)
       if start and finish then
         init = finish + 1
-        table.insert(highlights, { start = start, finish = finish })
+        table.insert(highlights, { start = start + offset, finish = finish + offset })
       end
     end
     return highlights
@@ -43,7 +59,7 @@ return sorters.Sorter:new {
       return 1
     end
     local res = regexps(prompt)
-    local display = entry.ordinal:lower()
+    local display = target(entry.ordinal):lower()
     return display:match(table.concat(res, ".*")) and entry.index or -1
   end,
 }
