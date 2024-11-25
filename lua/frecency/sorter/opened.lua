4876b6051c80e77ab8097e7a7dd3b26016a05d37
delphinus <me@delphinus.dev>
2024-11-25 15:03:19 +0900

    feat: precede relational buffers

--- lua/frecency/sorter/opened.lua
+++ lua/frecency/sorter/opened.lua
@@ -1,11 +1,32 @@
 local Default = require "frecency.sorter.default"
 
 ---@class FrecencySorterOpened: FrecencySorterDefault
+---@field protected buffers string[]
+---@field protected buffers_map table<string, boolean>
 local Opened = setmetatable({}, { __index = Default })
 
 ---@return FrecencySorterOpened
 Opened.new = function()
-  return setmetatable(Default.new(), { __index = Opened }) --[[@as FrecencySorterOpened]]
+  local self = setmetatable(Default.new(), { __index = Opened }) --[[@as FrecencySorterOpened]]
+  self.buffers = vim.tbl_map(vim.api.nvim_buf_get_name, vim.api.nvim_list_bufs())
+  self.buffers_map = {}
+  for _, buffer in ipairs(self.buffers) do
+    self.buffers_map[buffer] = true
+  end
+  return self
+end
+
+function Opened:sort(files)
+  local sorted = Default.sort(self, files)
+  ---@type FrecencyDatabaseEntry[], FrecencyDatabaseEntry[]
+  local result, others = {}, {}
+  for _, entry in ipairs(sorted) do
+    table.insert(self.buffers_map[entry.path] and result or others, entry)
+  end
+  for _, entry in ipairs(others) do
+    table.insert(result, entry)
+  end
+  return result
 end
 
 return Opened
--- lua/frecency/sorter/same_repo.lua
+++ lua/frecency/sorter/same_repo.lua
@@ -1,11 +1,44 @@
+local Default = require "frecency.sorter.default"
 local Opened = require "frecency.sorter.opened"
 
 ---@class FrecencySorterSameRepo: FrecencySorterOpened
+---@field private repos string[]
 local SameRepo = setmetatable({}, { __index = Opened })
 
 ---@return FrecencySorterSameRepo
 SameRepo.new = function()
-  return setmetatable(Opened.new(), { __index = SameRepo }) --[[@as FrecencySorterSameRepo]]
+  local self = setmetatable(Opened.new(), { __index = SameRepo }) --[[@as FrecencySorterSameRepo]]
+  self.repos = {}
+  for _, buf in ipairs(self.buffers) do
+    local repo = vim.fs.root(buf, ".git")
+    if repo then
+      table.insert(self.repos, repo)
+    end
+  end
+  return self
+end
+
+function SameRepo:sort(files)
+  local sorted = Default.sort(self, files)
+  if #self.repos == 0 then
+    return sorted
+  end
+  ---@type FrecencyDatabaseEntry[], FrecencyDatabaseEntry[]
+  local result, others = {}, {}
+  for _, entry in ipairs(sorted) do
+    local matched
+    for _, repo in ipairs(self.repos) do
+      matched = not not entry.path:find(repo, 1, true)
+      if matched then
+        break
+      end
+    end
+    table.insert(matched and result or others, entry)
+  end
+  for _, entry in ipairs(others) do
+    table.insert(result, entry)
+  end
+  return result
 end
 
 return SameRepo
